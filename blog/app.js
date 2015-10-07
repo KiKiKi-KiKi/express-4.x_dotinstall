11d8bc512701cd78fb9307f55c2c90dacf3d6e10
CHAIKA <chaika.design@gmail.com>
2015-10-07 16:28:56 +0900

    #13 index Template

--- blog/app.js
+++ blog/app.js
@@ -35,11 +35,12 @@ app.use(methodOverride(function(req, res){
 
 // Routing
 app.get('/', post.index);
+/*
 app.get('/posts/new', post.new);
 app.post('/posts/create', post.create);
 app.get('/posts/:id', post.show);
 app.get('/posts/:id/edit', post.edit);
 app.put('/posts/:id', post.update);
 app.delete('/posts/:id', post.destroy);
-
+*/
 app.listen(3000);
