93f50ce17042715b6c3c3c7131bf64f63e930202
CHAIKA <chaika.design@gmail.com>
2015-10-07 16:13:42 +0900

    #12 ルーティングの指定

--- blog/app.js
+++ blog/app.js
@@ -21,5 +21,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.use(app.router); // -> 'app.router' is deprecated!
 
 // Routing
+app.get('/', post.index);
+app.get('/posts/new', post.new);
+app.post('/posts/create', post.create);
+app.get('/posts/:id', post.show);
+app.get('/posts/:id/edit', post.edit);
+app.put('/posts/:id', post.update);
+app.delete('/posts/:id', post.destroy);
 
 app.listen(3000);
